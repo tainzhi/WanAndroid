a2606ee990327de1fbc05cde98f67ce8ced55d50
tainzhi <qfq61@qq.com>
2020-02-05 21:39:44 +0800

    fix: 从全面屏启动页到主页statusbar闪烁问题

--- app/src/main/java/com/tainzhi/android/wanandroid/ui/SplashActivity.kt
+++ app/src/main/java/com/tainzhi/android/wanandroid/ui/SplashActivity.kt
@@ -43,8 +43,19 @@ class SplashActivity : BaseActivity() {
     override fun initData() {
         launch {
             delay(3000L)
-            startKtxActivity<MainActivity>()
             finish()
+            startKtxActivity<MainActivity>()
         }
+    }
+
+    override fun finish() {
+        // bug: 从全面屏切换到非全面屏，状态栏会闪烁
+        // 方法1：退出前属性设置非全面屏。这种虽然貌似能解决问题，但是这样切换会导致你的
+        //       Activity在从全屏变化为非全屏时无法适应主题的变化，而被切掉一块
+        // window.clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN)
+        //        WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN)
+        // 方法2：清楚全屏属性
+        window.clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        super.finish()
     }
 }
