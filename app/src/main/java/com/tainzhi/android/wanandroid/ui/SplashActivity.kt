0f32ccd80334d563960fa798c5a2c0a905680b20
tainzhi <qfq61@qq.com>
2020-02-10 06:26:37 +0800

    引导页无法单activity化，所以取消fragment化

--- app/src/main/java/com/tainzhi/android/wanandroid/ui/SplashActivity.kt
+++ app/src/main/java/com/tainzhi/android/wanandroid/ui/SplashActivity.kt
@@ -1,22 +1,20 @@
 package com.tainzhi.android.wanandroid.ui
 
-import android.content.Context
 import android.text.Html
 import android.view.WindowManager
-import androidx.navigation.fragment.findNavController
 import com.tainzhi.android.wanandroid.R
 import com.tainzhi.android.wanandroid.adapter.SplashAdapter
-import com.tainzhi.android.wanandroid.base.ui.BaseFragment
+import com.tainzhi.android.wanandroid.base.ui.BaseActivity
 import com.tainzhi.android.wanandroid.binding.fromN
 import com.tainzhi.android.wanandroid.util.startKtxActivity
 import com.tainzhi.android.wanandroid.view.ScrollLinearLayoutManager
-import kotlinx.android.synthetic.main.splash_splash.*
+import kotlinx.android.synthetic.main.activity_splash.*
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 @ExperimentalCoroutinesApi
-class SplashFragment : BaseFragment() {
+class SplashActivity : BaseActivity() {
 
     private val imageSrcId = arrayListOf(
             R.drawable.android_0,
@@ -27,14 +25,14 @@ class SplashFragment : BaseFragment() {
             R.drawable.android_5)
     private val splashAdapter by lazy { SplashAdapter(imageSrcId) }
 
-    override fun getLayoutResId() = R.layout.splash_splash
+    override fun getLayoutResId() = R.layout.activity_splash
 
     override fun initView() {
-        activity?.window?.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN)
         splashRecyclerView.run {
             adapter = splashAdapter
-            layoutManager = ScrollLinearLayoutManager(activity as Context)
+            layoutManager = ScrollLinearLayoutManager(this@SplashActivity)
             smoothScrollToPosition(Integer.MAX_VALUE / 2)
         }
         splashLogoTv.text = if (fromN()) Html.fromHtml(getString(R.string.app_logo_html), Html.FROM_HTML_MODE_LEGACY)
@@ -45,19 +43,19 @@ class SplashFragment : BaseFragment() {
     override fun initData() {
         launch {
             delay(3000L)
-            onFinish()
+            finish()
             startKtxActivity<MainActivity>()
         }
     }
 
-    private fun onFinish() {
+    override fun finish() {
         // bug: 从全面屏切换到非全面屏，状态栏会闪烁
         // 方法1：退出前属性设置非全面屏。这种虽然貌似能解决问题，但是这样切换会导致你的
         //       Activity在从全屏变化为非全屏时无法适应主题的变化，而被切掉一块
         // window.clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN)
         //        WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN)
         // 方法2：清楚全屏属性
-        activity?.window?.clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
-        findNavController().popBackStack()
+        window.clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        super.finish()
     }
 }
