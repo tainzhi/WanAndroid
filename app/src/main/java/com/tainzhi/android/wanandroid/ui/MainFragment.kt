458ac6fa9cebc7d43978536fef8e7b1bec7a228c
tainzhi <qfq61@qq.com>
2020-02-21 14:03:14 +0800

    delete unused code

--- app/src/main/java/com/tainzhi/android/wanandroid/ui/MainFragment.kt
+++ app/src/main/java/com/tainzhi/android/wanandroid/ui/MainFragment.kt
@@ -1,7 +1,6 @@
 package com.tainzhi.android.wanandroid.ui
 
 import androidx.fragment.app.Fragment
-import androidx.navigation.Navigation
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayoutMediator
@@ -10,18 +9,9 @@ import com.tainzhi.android.wanandroid.base.ui.BaseFragment
 import com.tainzhi.android.wanandroid.ui.home.HomeFragment
 import com.tainzhi.android.wanandroid.ui.navigation.NavigationFragment
 import com.tainzhi.android.wanandroid.ui.system.SystemFragment
-import com.tainzhi.android.wanandroid.util.Preference
 import kotlinx.android.synthetic.main.fragment_main.*
 
-/**
- * A simple [Fragment] subclass.
- * Use the [MainFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class MainFragment : BaseFragment() {
-
-    private var isLogin by Preference(Preference.IS_LOGIN, false)
-
     private val titleList = arrayOf("首页", "广场", "最新项目", "体系", "导航")
     private val fragmentList = arrayListOf<Fragment>()
     private val homeFragment by lazy { HomeFragment() }
@@ -45,13 +35,6 @@ class MainFragment : BaseFragment() {
 
     override fun initView() {
         initViewPager()
-        addFab.setOnClickListener {
-            // TODO: 2020/1/28 add login 
-//            if (!isLogin)  Navigation.findNavController(viewPager).navigate(R.id.action_tab_to_login)
-//            else Navigation.findNavController(viewPager).navigate(R.id.action_tab_to_share)
-            Navigation.findNavController(viewPager).navigate(R.id
-                    .action_main_to_login)
-        }
     }
 
     override fun initData() {
@@ -75,7 +58,6 @@ class MainFragment : BaseFragment() {
         super.onResume()
         if (onPageChangeCallback == null) onPageChangeCallback = object : ViewPager2.OnPageChangeCallback() {
             override fun onPageSelected(position: Int) {
-                if (position == 1) addFab.show() else addFab.hide()
             }
         }
         onPageChangeCallback?.let { viewPager.registerOnPageChangeCallback(it) }
@@ -84,13 +66,6 @@ class MainFragment : BaseFragment() {
     override fun onStop() {
         super.onStop()
         onPageChangeCallback?.let { viewPager.unregisterOnPageChangeCallback(it) }
-    }
-
-
-    private fun refreshView() {
-//        navigationView.menu.findItem(R.id.nav_exit).isVisible = isLogin
-//        homeFragment.refresh()
-//        lastedProjectFragment.refresh()
     }
 
 }
