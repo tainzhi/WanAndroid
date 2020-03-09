174b86497180c0c8e5f2f313f737e3e9110e4dc8
tainzhi <qfq61@qq.com>
2020-03-10 00:07:48 +0800

    add AboutFragment

--- app/src/main/java/com/tainzhi/android/wanandroid/ui/SettingsFragment.kt
+++ app/src/main/java/com/tainzhi/android/wanandroid/ui/SettingsFragment.kt
@@ -1,24 +1,11 @@
 package com.tainzhi.android.wanandroid.ui
 
-import android.content.Intent
-import android.net.Uri
-import android.view.Gravity
-import android.view.MenuItem
-import android.widget.PopupMenu
 import androidx.activity.addCallback
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
-import com.tainzhi.android.wanandroid.BuildConfig
 import com.tainzhi.android.wanandroid.R
 import com.tainzhi.android.wanandroid.WanApp
 import com.tainzhi.android.wanandroid.base.ui.BaseFragment
-import com.tainzhi.android.wanandroid.util.GITHUB_PAGE
-import com.tainzhi.android.wanandroid.util.ISSUE_URL
-import com.tainzhi.android.wanandroid.util.openBrowser
-import com.tainzhi.android.wanandroid.view.MeDialog
-import de.psdev.licensesdialog.LicensesDialog
-import de.psdev.licensesdialog.licenses.ApacheSoftwareLicense20
-import de.psdev.licensesdialog.model.Notice
 import kotlinx.android.synthetic.main.common_toolbar.*
 import kotlinx.android.synthetic.main.fragment_settings.*
 
@@ -41,64 +28,13 @@ class SettingsFragment : BaseFragment() {
             isDarkTheme?.let { changeThemeSwitch.isChecked = it }
         })
 
-        licenseTv.setOnClickListener { showOwnLicense() }
-        sourceTv.setOnClickListener { activity?.openBrowser(GITHUB_PAGE) }
-        feedbackTv.setOnClickListener { showFeedBackMenu() }
-        thirdLibTv.setOnClickListener { showLicenseDialog() }
-        developerTv.setOnClickListener { showMe() }
-
-        versionNameTv.text = BuildConfig.VERSION_NAME
-    }
-
-    private fun showOwnLicense() {
-        val notice = Notice(getString(R.string.app_name), GITHUB_PAGE, "", ApacheSoftwareLicense20())
-        LicensesDialog.Builder(activity)
-                .setNotices(notice)
-                .build()
-                .show()
-    }
-
-    private fun showLicenseDialog() {
-        LicensesDialog.Builder(activity)
-                .setNotices(R.raw.licenses)
-                .build()
-                .show()
-    }
-
-    private fun showFeedBackMenu() {
-        val feedbackMenu = context?.let { PopupMenu(it, feedbackTv, Gravity.END) }
-        feedbackMenu?.menuInflater?.inflate(R.menu.feedback_menu, feedbackMenu.menu)
-        feedbackMenu?.setOnMenuItemClickListener { item: MenuItem ->
-            when (item.itemId) {
-                R.id.menu_issue -> {
-                    activity?.openBrowser(ISSUE_URL)
-                    true
-                }
-                R.id.menu_email -> {
-                    val uri = Uri.parse(getString(R.string.sendto))
-                    val intent = Intent(Intent.ACTION_SENDTO, uri)
-                    intent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.mail_topic))
-//                    intent.putExtra(Intent.EXTRA_TEXT,
-//                            getString(R.string.device_model) + Build.MODEL + "\n"
-//                                    + getString(R.string.sdk_version) + Build.VERSION.RELEASE + "\n"
-//                                    + getString(R.string.version))version
-                    startActivity(intent)
-                    true
-                }
-                else -> {
-                    true
-                }
-            }
+        settingsAboutFl.setOnClickListener {
+            findNavController().navigate(R.id.aboutFragment)
         }
-        feedbackMenu?.show()
     }
 
     private fun onBack() {
         findNavController().popBackStack(R.id.mainFragment, false)
-    }
-
-    private fun showMe() {
-        MeDialog.newInstance().show(childFragmentManager, getString(R.string.me))
     }
 }
 
---
+++ app/src/main/java/com/tainzhi/android/wanandroid/util/AppInfoUtils.kt
@@ -0,0 +1,12 @@
+package com.tainzhi.android.wanandroid.util
+
+/**
+ * @author:      tainzhi
+ * @mail:        qfq61@qq.com
+ * @date:        2020/3/9 下午10:23
+ * @description: AppInfo Utils
+ **/
+
+
+ 
+ 
