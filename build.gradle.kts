17d575ddfd637b50481dc28a6af5357bfa267cc8
tainzhi <qfq61@qq.com>
2020-12-22 19:31:19 +0800

    update github action

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,35 +1,29 @@
 import com.tainzhi.android.buildsrc.Libs
 
 buildscript {
-    val kotlin_version by extra("1.3.72")
     repositories {
-        
         google()
         jcenter()
-        
-        
-        dependencies {
-            classpath(com.tainzhi.android.buildsrc.Libs.androidGradlePlugin)
-            classpath(com.tainzhi.android.buildsrc.Libs.Kotlin.gradlePlugin)
-            classpath(com.tainzhi.android.buildsrc.Libs.AndroidX.Navigation.safeArgs)
-            classpath(com.tainzhi.android.buildsrc.Libs.buglyUploadMapping)
-        }
     }
-    
-    allprojects {
-        repositories {
-            google()
-            jcenter()
-            maven {
-                setUrl("https://jitpack.io")
-            }
-        }
-    }
-    
-    tasks.register("clean", Delete::class) {
-        delete(rootProject.buildDir)
-    }
+
     dependencies {
-        "classpath"("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
+        classpath(com.tainzhi.android.buildsrc.Libs.androidGradlePlugin)
+        classpath(com.tainzhi.android.buildsrc.Libs.Kotlin.gradlePlugin)
+        classpath(com.tainzhi.android.buildsrc.Libs.AndroidX.Navigation.safeArgs)
+        classpath(com.tainzhi.android.buildsrc.Libs.buglyUploadMapping)
     }
+
+}
+
+allprojects {
+    repositories {
+        google()
+        jcenter()
+        maven {
+            setUrl("https://jitpack.io")
+        }
+    }
+}
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
